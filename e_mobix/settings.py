d77b724ec0c0e887c2f26172fa84447d2c5538a0
Developer <dev@example.com>
2026-06-04 21:56:29 +0700

    Temporarily enable DEBUG to see error details

--- e_mobix/settings.py
+++ e_mobix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a8-@9fxjo6ba2w6)c&&=osp_h@nn5a)n&x43j_4_!_m462+(ab'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
