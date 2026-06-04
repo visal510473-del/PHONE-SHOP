5df5fc648cbadfb7586de4798b37af9093ff8574
Developer <dev@example.com>
2026-06-04 21:41:57 +0700

    Clean up hidden characters from urls

--- e_mobix/urls.py
+++ e_mobix/urls.py
@@ -17,46 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
-from django.conf import settings
-from django.views.static import serve
-from django.urls import path, re_path
-
-from cart.views import add_to_cart, cart_view
-from dashboard import views
-from dashboard.views import dashboard
-from accounts import views as accounts_views
-from products.views import home
-from cart import views as cart_views
-# pong
-from orders import views as orders_views
-# pong
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('products.urls')),
-    path('cart/', include('cart.urls')),
-    path('cart/', cart_views.cart_view, name='cart_view'),
-    path('accounts/', include('accounts.urls')),
-    path('dashboard/', include('dashboard.urls')),
-    # pong
-    path('checkout/', orders_views.checkout, name='checkout'),
-    path('cart/remove/<str:cart_key>/', cart_views.remove_from_cart, name='remove_from_cart'),
-    # pong
-    path('login/', accounts_views.login_view, name='login'),
-    path('register/', accounts_views.register_view, name='register'),
-    path('logout/', accounts_views.logout_view, name='logout'),
-​​​​​
-  
-  
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-    from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
